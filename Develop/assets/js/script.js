c7d74a9e92d03fb2b167368357ced932683c268e
bmallar <benmallar@gmail.com>
2024-03-08 18:04:16 -0600

    date picker working

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -16,7 +16,8 @@ function createTaskCard(task) {
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
-
+  
+ 
 }
 
 // Todo: create a function to handle adding a new task
@@ -44,7 +45,17 @@ function handleDeleteTask(event){
 
 // Todo: create a function to handle dropping a task into a new status lane
 function handleDrop(event, ui) {
-
+  $( function() {
+    $( "#draggable" ).draggable();
+    $( "#droppable" ).droppable({
+      drop: function( event, ui ) {
+        $( this )
+          .addClass( "ui-state-highlight" )
+         
+      }
+    });
+  } );
+  
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
@@ -55,6 +66,7 @@ $(document).ready(function () {
 
 $( function() {
   $( "#datepicker" ).datepicker();
+  
 } );
 
 $("#taskForm").on("submit", handleAddTask);
